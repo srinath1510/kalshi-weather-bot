aec18f9ffdb6c9ac78a461899e80d738a23c51dc
Srinath Srinivasan <srinath1510@gmail.com>
2026-01-24 11:00:23 -0500

    init

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -16,6 +16,15 @@ from src.data.station_parser import (
     determine_station_type,
 )
 
+from src.data.kalshi_client import (
+    KalshiMarketClient,
+    fetch_brackets_for_date,
+    get_market_summary,
+    parse_bracket_subtitle,
+    calculate_implied_probability,
+    format_date_for_ticker,
+)
+
 __all__ = [
     # Weather models
     "OpenMeteoSource",
@@ -29,4 +38,11 @@ __all__ = [
     "celsius_to_fahrenheit",
     "calculate_temp_bounds",
     "determine_station_type",
+    # Kalshi client
+    "KalshiMarketClient",
+    "fetch_brackets_for_date",
+    "get_market_summary",
+    "parse_bracket_subtitle",
+    "calculate_implied_probability",
+    "format_date_for_ticker",
 ]
