97ff2e5162b0b3f34c7aced141141aa46401e83d
Srinath Srinivasan <srinath1510@gmail.com>
2026-02-02 17:08:39 -0500

    fix importerror

--- kalshi_weather/__main__.py
+++ kalshi_weather/__main__.py
@@ -1,6 +1,6 @@
 """Entry point for running as a module: python -m kalshi_weather"""
 
-from kalshi_weather.cli import main
+from kalshi_weather.cli.commands import main
 
 if __name__ == "__main__":
     main()
